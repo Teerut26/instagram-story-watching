d32370ea5635fb60a6a583435f924d8706143a7b
Teerut26 <teerutsr@gmail.com>
2022-04-23 08:57:40 +0700

    init commit

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import _ from "lodash";
 export default class InstagramStoryWatching extends EventEmitter {
     private http_request = new HttpRequest();
     public last_story: Item | null = null;
-    private frist_run: boolean = false;
+    private first_run: boolean = false;
     private history_id: string[] = [];
 
     private url: string = "";
@@ -25,11 +25,11 @@ export default class InstagramStoryWatching extends EventEmitter {
             let last_data = _.last(res.data.reel?.items);
             if (res.data.reel !== null && this.last_story === null) {
                 this.last_story = last_data as Item;
-                if (this.frist_run) {
+                if (this.first_run) {
                     this.emit("item", last_data);
                     this.history_id.push(last_data?.id as string);
                 }
-                this.frist_run = true;
+                this.first_run = true;
             } else if (res.data.reel !== null) {
                 let result = last_data?.id === this.last_story?.id;
                 if (!result) {
@@ -43,9 +43,9 @@ export default class InstagramStoryWatching extends EventEmitter {
                         this.history_id.push(last_data?.id as string);
                     }
                 }
-                this.frist_run = true;
+                this.first_run = true;
             } else {
-                this.frist_run = true;
+                this.first_run = true;
             }
         }, 5000);
     }
